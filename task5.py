b6bc41542aeb736609a93af84c2642294ae458b7
Bledai <51114557+Bledai@users.noreply.github.com>
2019-06-24 11:50:50 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -3,7 +3,7 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    if (i % 3 == 0) and (i % 5 == 0):
+    if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
         continue
     elif i % 3 == 0:
