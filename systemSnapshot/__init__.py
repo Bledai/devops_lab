af939cb5e52fe673cab32afc68888698ca136d25
Anna Bledai <Bledai@localhost.localdomain>
2019-06-26 21:35:30 +0300

    code spase

--- systemSnapshot/__init__.py
+++ systemSnapshot/__init__.py
@@ -1,6 +1,6 @@
-from systemSnapshot.__main__ import Snapshot# noqa
-from systemSnapshot.cpuStatus import CpuStatus# noqa
-from systemSnapshot.netStatus import NetStatus# noqa
-from systemSnapshot.memoryStatus import MemoryStatus# noqa
-from systemSnapshot.virtualMemoryStatus import VirtualMemoryStatus# noqa
-from systemSnapshot.ioInform import IoInform# noqa
+from systemSnapshot.__main__ import Snapshot  # noqa
+from systemSnapshot.cpuStatus import CpuStatus  # noqa
+from systemSnapshot.netStatus import NetStatus  # noqa
+from systemSnapshot.memoryStatus import MemoryStatus  # noqa
+from systemSnapshot.virtualMemoryStatus import VirtualMemoryStatus  # noqa
+from systemSnapshot.ioInform import IoInform  # noqa
